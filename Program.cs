76f3391ece1983ecc4f62d985a56de269dac49bb
Zeta279 <equi27@live.com.ar>
2022-05-17 15:42:37 -0300

    Ejercicio 1 realizado

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿namespace TrabajoPractico5{
     class TrabajoPractico5{
         static void Main(string[] args){
-            Console.WriteLine("Hola mundo!");
+            ejercicio_1();
+        }
+
+        static void ejercicio_1(){
+            Console.Write("Ingrese un número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if(numero > 0){
+                var numInvertido = new String("");
+
+                foreach(char c in Convert.ToString(numero)){
+                    numInvertido = c + numInvertido;
+                }
+
+                Console.WriteLine("Número invertido: " + numInvertido);
+            }
+            else{
+                Console.WriteLine("Número original: " + Convert.ToString(numero));
+            }
+
         }
     }
 }
