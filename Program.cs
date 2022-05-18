72f17c45ee153222020d4c32ed9bf64fc168ff5c
Zeta279 <equi27@live.com.ar>
2022-05-17 21:30:20 -0300

    Añadido control para ingresar enteros

--- Program.cs
+++ Program.cs
@@ -2,11 +2,12 @@
     class TrabajoPractico5{
         static void Main(string[] args){
             ejercicio_1();
+            // Ejercicio 2, 3 y 4 realizado en otras ramas
         }
 
         static void ejercicio_1(){
             Console.Write("Ingrese un número: ");
-            int numero = Convert.ToInt32(Console.ReadLine());
+            int numero = ControlIngreso.IngresarInt();
 
             if(numero > 0){
                 var numInvertido = new String("");
@@ -21,6 +22,25 @@
                 Console.WriteLine("Número original: " + Convert.ToString(numero));
             }
 
+        }
+    }
+
+    class ControlIngreso{
+        public static int IngresarInt(){
+            int entero = 0;
+            bool error = true;
+
+            while(error){
+                try{
+                    entero = Convert.ToInt32(Console.ReadLine());
+                    error = false;
+                }
+                catch{
+                    Console.WriteLine("Solo se permite ingresar números");
+                }
+            }
+
+            return entero;
         }
     }
 }
